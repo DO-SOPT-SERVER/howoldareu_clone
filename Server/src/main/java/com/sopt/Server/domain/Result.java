139124158b4b0835dea0d7b1d4c644582c42bf9a
sung-silver <jinna0319@gmail.com>
2023-11-25 23:52:41 +0900

    [FEAT] 질문 조회 API 개발

--- Server/src/main/java/com/sopt/Server/domain/Result.java
+++ Server/src/main/java/com/sopt/Server/domain/Result.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Table(name = "RESULTS")
 public class Result {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "result_id")
     private Long id;
 
