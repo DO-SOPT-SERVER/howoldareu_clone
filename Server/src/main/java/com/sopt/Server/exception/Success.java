9fbc703754dc6756247c1c02374811385cdf2954
Parkjyun <jaeyun0507@naver.com>
2023-11-26 03:41:00 +0900

    [FEAT] 누적결과 api반환

--- Server/src/main/java/com/sopt/Server/exception/Success.java
+++ Server/src/main/java/com/sopt/Server/exception/Success.java
@@ -11,6 +11,7 @@ public enum Success {
     GET_HEALTH_CHECK_SUCCESS(HttpStatus.OK, "서버 상태 체크에 성공했습니다"),
     GET_MEMBER_SUCCESS(HttpStatus.OK, "멤버 조회에 성공했습니다"),
     GET_QUESTION_LIST_SUCCESS(HttpStatus.OK, "질문 리스트 조회 성공"),
+    GET_USER_LIST_SUCCESS(HttpStatus.OK, "유저 결과 리스트 반환 성공"),
 
     // 201 created
     CREATE_MEMBER_SUCCESS(HttpStatus.CREATED, "멤버 생성에 성공했습니다"),
