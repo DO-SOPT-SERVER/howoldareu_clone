c8a23ea2e8bd5425cd4582358ede9e05683ea577
unanchoi <unanc2@gmail.com>
2024-03-27 20:25:56 +0900

    2주차 테스트코드 작성

--- src/test/java/com/sopt/Server/service/ResultServiceTest.java
+++ src/test/java/com/sopt/Server/service/ResultServiceTest.java
@@ -6,16 +6,17 @@ import com.sopt.Server.controller.response.ResultResponse;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ResultServiceTest {
 
     @Mock
@@ -54,6 +55,45 @@ public class ResultServiceTest {
         Assertions.assertThat(result)
                 .extracting("nickname", "resultAge", "title", "content", "imgUrl1", "imgUrl2")
                 .contains("testNickname", 20, "testTitle", "testContent", "testImgUrl1", "testImgUrl2");
+
+    }
+
+    @Test
+    @DisplayName("모든 결과를 조회할 수 있다.")
+    void getAllResults() throws Exception {
+        // given
+        final long memberId = 1L;
+        ResultResponse response1 = ResultResponse.of(
+                "testNickname1",
+                20,
+                "testTitle1",
+                "testContent1",
+                "testImgUrl1",
+                "testImgUrl2"
+        );
+
+        ResultResponse response2 = ResultResponse.of(
+                "testNickname2",
+                30,
+                "testTitle2",
+                "testContent2",
+                "testImgUrl1",
+                "testImgUrl2"
+        );
+
+        BDDMockito.given(resultService.getAllResults(anyLong()))
+                .willReturn(List.of(response1, response2));
+
+      // when
+        List<ResultResponse> response = resultService.getAllResults(memberId);
+
+      // then
+        Assertions.assertThat(response)
+                .extracting("nickname", "resultAge", "title", "content", "imgUrl1", "imgUrl2")
+                .containsExactlyInAnyOrder(
+                        Assertions.tuple("testNickname1", 20, "testTitle1", "testContent1", "testImgUrl1", "testImgUrl2"),
+                        Assertions.tuple("testNickname2", 30, "testTitle2", "testContent2", "testImgUrl1", "testImgUrl2")
+                );
 
     }
 }
